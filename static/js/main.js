5251cad2660519998a695e933aac1d5910f55d09
GianisTsol <35958401+GianisTsol@users.noreply.github.com>
2022-05-03 16:59:29 +0300

    Made ui more responsive at performance cost. TODO: move from json to db.

--- static/js/main.js
+++ static/js/main.js
@@ -173,21 +173,31 @@ function addFood(id) {
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function ToggleDropdown() {
-$("#myDropdown").toggle();
+  $("#myDropdown").toggle();
+  filterFunction();
 }
 
 function filterFunction() {
-var input, filter, ul, li, a, i;
-input = document.getElementById("myInput");
-filter = input.value.toUpperCase();
-div = document.getElementById("myDropdown");
-a = div.getElementsByTagName("a");
-for (i = 0; i < a.length; i++) {
-  txtValue = a[i].textContent || a[i].innerText;
-  if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    a[i].style.display = "";
-  } else {
-    a[i].style.display = "none";
+  var children = $('#inputs').children();
+  var added = [];
+  children.each(function(i) {
+    const id = $(this).attr("__item");
+    if ($(this).attr("__item") != null) {
+        added.push($(`#option-${id}`).text());
+    }
+  });
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1 && added.indexOf(txtValue) == -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
   }
-}
+
 }
